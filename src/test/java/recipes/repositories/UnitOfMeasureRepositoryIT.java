143fdf09b24639af5c29f892bf9ea6218e6f491d
o.O <descatalogado@gmail.com>
2021-08-27 20:08:38 +0200

    10.10 Integration test con Spring Context

---
+++ src/test/java/recipes/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,45 @@
+package recipes.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import recipes.domain.UnitOfMeasure;
+
+//@RunWith(SpringRunner.class) ya no es necesario en JUnit 5
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    //@DirtiesContext forzaría a recargar el contexto al terminar el test
+    public void findByDescription() throws Exception {
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+
+    @Test
+    public void findByDescriptionCup() throws Exception {
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+
+}
